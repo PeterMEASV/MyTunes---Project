32f3d194089f6f7bd9659ed7f6890113b64227bb
Svoldgaard <Svoldgaard96@gmail.com>
2024-11-26 09:43:57 +0100

    gui.v1

--- src/main/java/personalprojects/mytunesproject/gui/MyTunesController.java
+++ src/main/java/personalprojects/mytunesproject/gui/MyTunesController.java
@@ -3,7 +3,7 @@ package personalprojects.mytunesproject.gui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MyTunesController {
     @FXML
     private Label welcomeText;
 
