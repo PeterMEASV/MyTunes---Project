85c7c76681c3802817b3130bc3592c9248eab056
Peteris Meiris <petmei01@easv365.dk>
2024-11-27 10:16:15 +0100

    Added file selection for creation of song

--- src/main/java/personalprojects/mytunesproject/gui/NewSongController.java
+++ src/main/java/personalprojects/mytunesproject/gui/NewSongController.java
@@ -4,7 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class NewSongController {
     @FXML
@@ -26,6 +29,14 @@ public class NewSongController {
     }
     @FXML
     private void btnChooseFile(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Music file");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Music Files", "*.mp3","*.wav"));
+        File file = fileChooser.showOpenDialog(new Stage());
+        if (file != null) {
+            txtFileName.setText(file.getAbsolutePath());
+        }
+
     }
     @FXML
     private void btnCancelNewSong(ActionEvent actionEvent) {
